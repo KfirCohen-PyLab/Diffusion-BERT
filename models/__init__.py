6d4dfccbd39460c48305c4bd1a27811e0a638eb7
KfirCohen-PyLab <kfirn13@gmail.com>
2025-05-29 18:48:41 +0300

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+# Make models directory a Python package
+from .modeling_diffusion_bert import DiffusionBertForMaskedLM
+from .modeling_bert import *
+from .modeling_bert_new_timestep import *
+from .modeling_roberta import * 
